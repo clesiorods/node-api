74e9b41337391846cbe7488267acad338617f331
clesiorods <clesiorodrigues96@gmail.com>
2020-12-02 10:37:50 -0300

    Projeto Nodejs Rocketseat finalizado

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 
 //INICIANDO O APP
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 // INICIANDO O BANCO DE DADOS
 mongoose.connect('mongodb://localhost:27017/node-api', { useUnifiedTopology: true, useNewUrlParser: true } );
